650877e412f1a6642844d0e566326f5bb8898359
fuuchin <86580653+fuuchin@users.noreply.github.com>
2021-08-31 16:41:10 +0900

    Update btn.js

--- js/btn.js
+++ js/btn.js
@@ -8,3 +8,32 @@ registerBtn.onmouseup = () => {
   registerBtn.style.boxShadow = '5px 5px 2px #000';
   registerBtn.style.transform = 'translate(0, 0)';
 }
+
+const resetBtn = id('reset');
+
+resetBtn.onmousedown = () => {
+  resetBtn.style.boxShadow = 'none'; //マウスを下げると 影が消える
+  resetBtn.style.transform = 'translate(1px, 2px)'; //ボタンが動く
+}
+
+resetBtn.onmouseup = () => {
+  //マウスがホバーした状態に戻ると cssで書いた通りに元に戻る
+  resetBtn.style.boxShadow = '5px 5px 2px #000';
+  resetBtn.style.transform = 'translate(0, 0)';
+}
+
+resetBtn.onclick = () => {
+  window.location.reload();
+}
+
+
+copyBtn.onmousedown = () => {
+  copyBtn.style.boxShadow = 'none'; //マウスを下げると 影が消える
+  copyBtn.style.transform = 'translate(1px, 2px)'; //ボタンが動く
+}
+
+copyBtn.onmouseup = () => {
+  //マウスがホバーした状態に戻ると cssで書いた通りに元に戻る
+  copyBtn.style.boxShadow = '5px 5px 2px #000';
+  copyBtn.style.transform = 'translate(0, 0)';
+}
